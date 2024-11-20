02298650daf70bb64aae7ce77924e48223179a2a
harrylbi <122418589+harrylbi@users.noreply.github.com>
2024-11-20 14:16:52 +0700

    Create recover.go

---
+++ basic/recover.go
@@ -0,0 +1,26 @@
+// recover bisa mendapatakan data panic
+
+package main
+
+import (
+	"fmt"
+
+	
+)
+
+ func endApp(){
+	fmt.Println("end app")
+	message := recover()
+	fmt.Println("terjadi panic", message)
+ }
+
+ func runApp(error bool){
+	defer endApp()
+	if error {
+		panic("ups error")
+	}
+ }
+
+ func main(){
+	runApp(true)
+ }
