0b908daecd3ab725d6a4b752cb03f8e67e5924ea
harrylbi <122418589+harrylbi@users.noreply.github.com>
2024-11-23 00:20:40 +0700

    Create resulting.go

---
+++ basic/resulting.go
@@ -0,0 +1,14 @@
+pacakge main
+
+imporrt "fmt"
+
+func Nyoba() any{
+	return "ok"
+}
+
+func main(){
+    var result any = random()
+	var resulting string = result.(string)
+	fmt.Println(resulting)
+	
+}
